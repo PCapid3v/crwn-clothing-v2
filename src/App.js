6107bdd1f0c77130558212bd3bc21e092cdcffed
PCapid3v <velenum@gmail.com>
2022-04-07 12:01:31 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
-import Directory from "./components/directory/directory.component";
+import { Routes,Route } from "react-router-dom";
 
-const App = () => {
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
-  const categories =[
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+const Shop = () => {
+    return(<h1>I am the Shop page</h1>)
   }
-]
-
-
+/* do not put the first <Route path> as 
+self closing tag otherwise you can't use 
+the route element and it won't work*/
+const App = () => {
   return (
-    <Directory categories={categories} />
-  );
+  <Routes >
+    <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+    </Route>
+    </Routes> 
+  )
 };
 
 export default App;
